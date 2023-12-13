7894ca1fc13818253b5a070666e18f3a7ed950c1
Raszero <travisjcelli95@gmail.com>
2023-12-13 08:21:59 -0600

    create role function

--- db/schema.sql
+++ db/schema.sql
@@ -7,24 +7,24 @@ USE companyEmployee_db;
 
 -- departments
 CREATE TABLE departments (
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 -- roles
 CREATE TABLE roles (
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES departments(department_id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 -- employees
 CREATE TABLE employees (
-     employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      role_id INT,
      first_name VARCHAR(30) NOT NULL,
      last_name VARCHAR(30) NOT NULL,
--- server.js
+++ server.js
@@ -16,11 +16,12 @@ function start() {
         "View Roles",
         "View Employees",
         "Create New Department",
+        "Create New Role",
       ],
     })
     .then((answer) => {
       switch (answer.action) {
-        case "view Departments":
+        case "View Departments":
           viewDepartments();
           break;
         case "View Roles":
@@ -31,6 +32,9 @@ function start() {
           break;
         case "Create New Department":
           createDepartment();
+          break;
+        case "Create New Role":
+          createRole();
           break;
       }
     });
@@ -67,14 +71,9 @@ function viewDepartments() {
 //salary -- check
 
 function viewRoles() {
-  const query = `
-  SELECT r.role_id, 
-  r.title, 
-  r.salary, 
-  d.department_name 
+  const query = `SELECT r.role_id, r.title, r.salary, d.department_name
   FROM roles r
-  JOIN departments d
-  `;
+  JOIN departments d ON r.department_id = d.department_id`;
   con.query(query, (err, res) => {
     if (err) throw err;
     console.table(res);
@@ -99,18 +98,18 @@ function viewRoles() {
 
 function viewEmployees() {
   const query = `
-  SELECT e.employee_id, 
+  SELECT 
+  e.id, 
   e.first_name, 
   e.last_name, 
   r.title, 
-  d.department_name, 
+  d.department_name,
   r.salary,
-  CONCAT(m.first_name, " ", m.last_name)
-  AS manager_name
+  CONCAT(m.first_name, " ", m.last_name) AS manager_name
   FROM employees e
-  JOIN roles r
-  JOIN departments d
-  JOIN employees m`;
+  LEFT JOIN roles r ON e.role_id = r.id
+  LEFT JOIN departments d ON r.department_id = d.id
+  LEFT JOIN employees m on e.manager_id = m.id`;
   con.query(query, (err, res) => {
     if (err) throw err;
     console.table(res);
@@ -125,40 +124,99 @@ function viewEmployees() {
 //logic
 // create a function that will allow user to add a department to the database
 
-//Add to start() inquirer
+//Add to start() inquirer -- check
 
 function createDepartment() {
-  // set inquirer to prompt for name
+  // set inquirer to prompt for name --check
   inquirer
     .prompt({
       type: "input",
-      name: "name",
+      name: "department",
       message: "What is the name of the new department?",
     })
-    //insert into seeds.sql
+    //insert into seeds.sql --check
     .then((answer) => {
-      console.log(answer.name);
-      //answer.name is working 
-      //query to insert name into department seeds
+      console.log(answer.department);
       const query = `
       INSERT INTO departments (department_name)
-      VALUE ('${answer.name}')`;
+      VALUE ('${answer.department}')`;
       con.query(query, (err, res) => {
-        if (err) { 
-          throw err
+        if (err) {
+          throw err;
         } else {
-          console.log(`Department: ${answer.name} has been created successfully!`)
+          console.log(
+            `Department: ${answer.department} has been created successfully!`
+          );
         }
         start();
-      })
+      });
     });
 }
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
+// logic
+// create a function that allows user to create a new role
+// name
+// salary
+// and the department role is in.
+
+//add to start() inquirer -- check
+// set inquirer to prompt for roll name
+// insert into seeds
+function createRole() {
+  const query = `SELECT * FROM departments`;
+  con.query(query, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    inquirer
+      .prompt([
+        // TODO: Why did this prompt have to be passed as a array?
+        {
+          type: "input",
+          name: "title",
+          message: "What is the name of the new Role?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary of this role?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Select the appropriate department",
+          choices: res.map((department) => department.department_name),
+        },
+      ])
+      .then((answer) => {
+        const department = res.find(
+          (department) => department.name === answer.department
+        );
+        const query = `INSERT INTO roles SET ?`;
+        con.query(
+          query,
+          {
+            title: answer.title,
+            salary: answer.salary,
+            department_id: department,
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log(`
+            Added role "${answer.title}" with a salary of ${answer.salary} to the department ${answer.department}`);
+          }
+        );
+      });
+  });
+  start();
+}
+
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
